00ea213a65a39ebd8e49343fdd9ddf55444f234b
lee-LAB <jiho264@naver.com>
2024-08-19 16:30:57 +0900

    copy softmax, ln, gelu from i-vit

--- utils/quant_modules.py
+++ utils/quant_modules.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from torch import Tensor
 from typing import Optional, Tuple
 from .quant_utils import AbsMaxQuantizer, MovAvgAbsMaxQuantizer
+from torch.autograd import Function
 
 
 class QuantAct(nn.Module):
@@ -66,17 +67,90 @@ class QuantLinearWithWeight(nn.Module):
         return x, s_in
 
 
+class floor_ste(Function):
+    """
+    Straight-through Estimator(STE) for torch.floor()
+    """
+
+    @staticmethod
+    def forward(ctx, x):
+        return torch.floor(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.clone()
+
+
+class round_ste(Function):
+    """
+    Straight-through Estimator(STE) for torch.round()
+    """
+
+    @staticmethod
+    def forward(ctx, x):
+        return torch.round(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.clone()
+
+
 class IntGELU(nn.Module):
     def __init__(self, args_gelu):
         super().__init__()
         self.do_quant = False
-        # if args_gelu.get("scheme") in ["I-ViT"]:
 
-        #     pass
-        # else:
-        #     raise NotImplementedError
+        self.output_bit = args_gelu.get("output_bit", 8)
+
+        self.n = 23  # sufficiently large integer
+        # The minimum value for ensuring accuracy (varies depending on models)
+
+        self.register_buffer("act_scaling_factor", torch.zeros(1))
+
+    def int_exp_shift(self, x_int, scaling_factor):
+        x_int = x_int + floor_ste.apply(x_int / 2) - floor_ste.apply(x_int / 2**4)
+
+        with torch.no_grad():
+            x0_int = torch.floor(-1.0 / scaling_factor)
+        x_int = torch.max(x_int, self.n * x0_int)
+
+        q = floor_ste.apply(x_int / x0_int)
+        r = x_int - x0_int * q
+        exp_int = r / 2 - x0_int
+        exp_int = torch.clamp(floor_ste.apply(exp_int * 2 ** (self.n - q)), min=0)
+        scaling_factor = scaling_factor / 2**self.n
+
+        return exp_int, scaling_factor
+
+    def int_forward(self, x, scaling_factor=None):
+        pre_x_int = x / scaling_factor
+        scaling_factor_sig = scaling_factor * 1.702
+
+        x_int_max, _ = pre_x_int.max(dim=-1, keepdim=True)
+        x_int = pre_x_int - x_int_max
+
+        exp_int, _ = self.int_exp_shift(x_int, scaling_factor_sig)  # e^(x-x_max)
+
+        exp_int_max, _ = self.int_exp_shift(
+            -x_int_max, scaling_factor_sig
+        )  # e^(-x_max)
+        exp_int_sum = exp_int + exp_int_max
+
+        exp_int_sum.clamp_max_(2**31 - 1)
+        factor = floor_ste.apply((2**31 - 1) / exp_int_sum)
+        sigmoid_int = floor_ste.apply(
+            exp_int * factor / 2 ** (31 - self.output_bit + 1)
+        )
+        sigmoid_scaling_factor = torch.Tensor([1 / 2 ** (self.output_bit - 1)]).cuda()
+
+        x_int = pre_x_int * sigmoid_int
+        scaling_factor = scaling_factor * sigmoid_scaling_factor
+        self.act_scaling_factor = scaling_factor
+        return x_int * scaling_factor, scaling_factor
 
     def forward(self, input, s_in=1):
+        if self.do_quant:
+            return self.int_forward(input, s_in)
         return F.gelu(input), s_in
 
 
@@ -84,14 +158,47 @@ class IntSoftMax(nn.Module):
     def __init__(self, args_softmax):
         super().__init__()
         self.do_quant = False
-        # if args_softmax.get("scheme") in ["I-ViT"]:
-        #     self.N = 15
-        #     self.M = 1
-        #     pass
-        # else:
-        #     raise NotImplementedError
+
+        self.output_bit = args_softmax.get("output_bit", 8)
+
+        self.n = 15  # sufficiently large integer
+        # The minimum value for ensuring accuracy (varies depending on models)
+
+        self.register_buffer("act_scaling_factor", torch.zeros(1))
+
+    def int_exp_shift(self, x_int, scaling_factor):
+        x_int = x_int + floor_ste.apply(x_int / 2) - floor_ste.apply(x_int / 2**4)
+
+        with torch.no_grad():
+            x0_int = torch.floor(-1.0 / scaling_factor)
+        x_int = torch.max(x_int, self.n * x0_int)
+
+        q = floor_ste.apply(x_int / x0_int)
+        r = x_int - x0_int * q
+        exp_int = r / 2 - x0_int
+        exp_int = torch.clamp(floor_ste.apply(exp_int * 2 ** (self.n - q)), min=0)
+        scaling_factor = scaling_factor / 2**self.n
+        return exp_int, scaling_factor
+
+    def int_forward(self, x, scaling_factor):
+        x_int = x / scaling_factor
+        x_int_max, _ = x_int.max(dim=-1, keepdim=True)
+        x_int = x_int - x_int_max
+
+        exp_int, _ = self.int_exp_shift(x_int, scaling_factor)
+        exp_int_sum = exp_int.sum(dim=-1, keepdim=True)
+
+        exp_int_sum.clamp_max_(2**31 - 1)
+        factor = floor_ste.apply((2**31 - 1) / exp_int_sum)
+        exp_int = floor_ste.apply(exp_int * factor / 2 ** (31 - self.output_bit + 1))
+        scaling_factor = torch.Tensor([1 / 2 ** (self.output_bit - 1)]).cuda()
+
+        self.act_scaling_factor = scaling_factor
+        return exp_int * scaling_factor, scaling_factor
 
     def forward(self, input, s_in=1, dim: int = -1):
+        if self.do_quant:
+            return self.int_forward(input, s_in)
         return F.softmax(input, dim=dim), s_in
 
 
@@ -99,13 +206,51 @@ class IntLayerNorm(nn.Module):
     def __init__(self, orgModule, args_ln):
         super().__init__()
         self.do_quant = False
-        # if args_ln.get("scheme") in ["I-ViT"]:
-        #     pass
-        # else:
-        #     raise NotImplementedError
+
         self.weight = orgModule.weight.clone().detach()
         self.bias = orgModule.bias.clone().detach()
         self.orgModule = orgModule
 
+        self.dim_sqrt = None
+        self.register_buffer("norm_scaling_factor", torch.zeros(1))
+        self.register_buffer("bias_integer", torch.zeros_like(self.bias))
+
+    def forward(self, x, scaling_factor=None):
+        if self.dim_sqrt is None:
+            n = torch.tensor(x.shape[2], dtype=torch.float)
+            self.dim_sqrt = torch.sqrt(n).cuda()
+
+        # Normalization: computes mean and variance(std)
+        x_int = x / scaling_factor
+        mean_int = round_ste.apply(x_int.mean(axis=2, keepdim=True))
+        y_int = x_int - mean_int
+        y_sq_int = y_int**2
+        var_int = torch.sum(y_sq_int, axis=2, keepdim=True)
+
+        # Integer Iteration
+        k = 2**16
+        for _ in range(10):
+            k_1 = floor_ste.apply((k + floor_ste.apply(var_int / k)) / 2)
+            k = k_1
+        std_int = k
+
+        factor = floor_ste.apply((2**31 - 1) / std_int)
+        y_int = floor_ste.apply(y_int * factor / 2)
+        scaling_factor = self.dim_sqrt / 2**30
+
+        # scaling and shifting
+        bias = self.bias.data.detach() / (self.weight.data.detach())
+        bias_int = floor_ste.apply(bias / scaling_factor)
+
+        self.bias_integer = bias_int
+
+        y_int = y_int + bias_int
+        scaling_factor = scaling_factor * self.weight
+        x = y_int * scaling_factor
+        self.norm_scaling_factor = scaling_factor
+        return x, scaling_factor
+
     def forward(self, input, s_in=1):
+        if self.do_quant:
+            return self.int_forward(input, s_in)
         return self.orgModule(input), s_in
